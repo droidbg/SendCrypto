406675b52cabc60e805329bf8094b89a3fb947f2
Binni Goel <binni.goel@thoughtworks.com>
2022-09-19 10:30:51 +0530

    [Binni] Add. starting code

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import {
+  Loader,
+  Welcome,
+  Footer,
+  Navbar,
+  Services,
+  Transactions,
+} from "./components";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
