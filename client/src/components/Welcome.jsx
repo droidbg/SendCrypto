ceeb0967b7d3f50798d3b624c3a5ed21a0cc8902
Binni Goel <binni1108@gmail.com>
2022-10-28 11:06:44 +0530

    [Binni] Add. Transaction section complete

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { TransactionContext } from "../context/TransactionContext";
-import { shortenAddress } from "../utils/shortenAddress.js ";
+import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
 const companyCommonStyles =
